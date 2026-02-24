e780e9a0ecbc4f5d315a5afdf82f668d8e3a76fc
Iván García <108953442+datcosmi@users.noreply.github.com>
2026-02-23 23:45:13 -0600

    Replace Hyprland Polkit Agent with Polkit GNOME Agent

--- hosts/suavicrema/modules/services.nix
+++ hosts/suavicrema/modules/services.nix
@@ -14,13 +14,15 @@
 
   security.polkit.enable = true;
 
-  systemd.user.services.hyprpolkitagent = {
-    description = "Hyprland Polkit Authentication Agent";
-    wantedBy = ["graphical-session.target"];
-    after = ["graphical-session.target"];
+  systemd.user.services.polkit-gnome-authentication-agent-1 = {
+    description = "polkit-gnome-authentication-agent-1";
+    wantedBy    = [ "graphical-session.target" ];
+    wants       = [ "graphical-session.target" ];
+    after       = [ "graphical-session.target" ];
     serviceConfig = {
-      ExecStart = "${pkgs.hyprpolkitagent}/libexec/hyprpolkitagent";
-      Restart = "on-failure";
+      Type      = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart   = "on-failure";
     };
   };
 
