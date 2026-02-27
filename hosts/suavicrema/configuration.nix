c45976b1b7116bc404e088f83ccbe1cc5e10e7a9
Iván García <garcia.cli@pm.me>
2026-02-26 22:30:42 -0600

    add hyprland to cachix

--- hosts/suavicrema/configuration.nix
+++ hosts/suavicrema/configuration.nix
@@ -17,10 +17,6 @@
   nix.settings = {
     experimental-features = "nix-command flakes";
     auto-optimise-store = true;
-
-    substituters = ["https://cache.nixos.org/" "https://hyprland.cachix.org" "https://niri.cachix.org"];
-    trusted-substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
   };
 
   system.autoUpgrade = {
