b653c2cbec89bc2e3ceb7647a22e5fae8829d971
Iván García <garcia.cli@pm.me>
2026-03-03 20:37:54 -0600

    users rearrengement

--- hosts/suavicrema/default.nix
+++ hosts/suavicrema/default.nix
@@ -3,11 +3,13 @@
     ../../profiles/desktop.nix
     ./hardware-configuration.nix
     ./disko.nix
+
+    ../../modules/system/users/ivan.nix
   ];
 
   networking.hostName = "suavicrema";
   my.hardware.gpu = "nvidia";
-  home-manager.users.ivan = import ./home/ivan;
+  home-manager.users.ivan = import ../../home/ivan;
 
   system.stateVersion = "26.05";
 }
