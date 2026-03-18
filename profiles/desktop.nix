47fdcd43b086a71a6c8de8252bd72244e3aae21c
Iván García <garcia.cli@pm.me>
2026-03-17 22:16:50 -0600

    cliphist module fix & new desktop modules structure

--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -8,11 +8,9 @@
     ../modules/system/memory.nix
     ../modules/system/utils
     ../modules/system/display.nix
-    ../modules/system/shell.nix
     ../modules/system/login/tuigreet.nix
 
     ../modules/system/desktop
-    ../modules/system/gaming.nix
     ../modules/system/cachix.nix
   ];
 }
