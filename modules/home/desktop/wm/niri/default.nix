f677b03b798b3e0e6876715c06f5bf35f1cbea0a
Iván García <garcia.cli@pm.me>
2026-03-20 20:48:01 -0600

    niri config per host

--- modules/home/desktop/wm/niri/default.nix
+++ modules/home/desktop/wm/niri/default.nix
@@ -1,15 +1,16 @@
 {
   config,
   lib,
+  osConfig,
   ...
 }: let
-  hostType = config.my.hardware.host;
+  hostType = osConfig.my.hardware.host;
 in {
   imports = [
     ./lock.nix
   ];
 
-  xdg.configFile."niri/config.kdl".text = lib.concatStringsSep "/n" [
+  xdg.configFile."niri/config.kdl".text = lib.concatStringsSep "\n" [
     (builtins.readFile ./base.kdl)
     (builtins.readFile (./monitors + "/${hostType}.kdl"))
     (builtins.readFile (./inputs + "/${hostType}.kdl"))
