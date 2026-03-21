378074bd2662f3cbb07075214b6b815c25bb3cb0
Iván García <garcia.cli@pm.me>
2026-03-20 21:42:47 -0600

    niri config per host

--- modules/home/desktop/wm/niri/default.nix
+++ modules/home/desktop/wm/niri/default.nix
@@ -5,14 +5,17 @@
   ...
 }: let
   hostType = osConfig.my.hardware.host;
+  cfg = osConfig.my.features.desktop.niri;
 in {
   imports = [
     ./lock.nix
   ];
 
-  xdg.configFile."niri/config.kdl".text = lib.concatStringsSep "\n" [
-    (builtins.readFile ./base.kdl)
-    (builtins.readFile (./monitors + "/${hostType}.kdl"))
-    (builtins.readFile (./inputs + "/${hostType}.kdl"))
-  ];
+  config = lib.mkIf cfg.enable {
+    xdg.configFile."niri/config.kdl".text = lib.concatStringsSep "\n" [
+      (builtins.readFile ./base.kdl)
+      (builtins.readFile (./monitors + "/${hostType}.kdl"))
+      (builtins.readFile (./inputs + "/${hostType}.kdl"))
+    ];
+  };
 }
--- modules/home/desktop/wm/niri/lock.nix
+++ modules/home/desktop/wm/niri/lock.nix
@@ -1,40 +1,33 @@
 {
-  config,
   pkgs,
   lib,
+  osConfig,
   ...
 }: let
   lockCmd = "${pkgs.swaylock-effects}/bin/swaylock --screenshots --clock --indicator --effect-blur 7x5";
-  cfg = config.my.wm;
 in {
-  config = lib.mkIf cfg.niri {
+  config = lib.mkIf osConfig.my.features.desktop.niri.enable {
     home.packages = with pkgs; [
-      # swaylock-effects
       hyprlock
       swayidle
     ];
-
     services.swayidle = {
       enable = true;
-
       timeouts = [
         {
           timeout = 600;
           command = "${pkgs.hyprlock}/bin/hyprlock";
         }
-
         {
           timeout = 900;
           command = "niri msg action dpms off";
           resumeCommand = "niri msg action dpms on";
         }
-
         {
           timeout = 1200;
           command = "${pkgs.hyprlock}/bin/hyprlock && systemctl suspend";
         }
       ];
-
       events = {
         before-sleep = "${pkgs.hyprlock}/bin/hyprlock";
       };
